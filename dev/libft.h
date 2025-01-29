722789af31f78967919dba4d2ee2582ef23fc4fd
aramarak <aramarak@student.42yerevan.am>
2025-01-29 21:49:26 +0400

    norminette fix

--- dev/libft.h
+++ dev/libft.h
@@ -6,12 +6,13 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 19:02:38 by aramarak          #+#    #+#             */
-/*   Updated: 2025/01/21 19:34:03 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/01/29 21:46:09 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <stddef.h>
 
 // Part 1 - Libc functions
 int	ft_isalpha(int c);
