c0d4cbcb1d8b3674acece7989458ec1d20694ef4
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-02-02 16:28:11 +0400

    modified: ft_strlcat.c

--- dev/ft_strlcat.c
+++ dev/ft_strlcat.c
@@ -6,15 +6,30 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 17:58:17 by aramarak          #+#    #+#             */
-/*   Updated: 2025/02/01 18:12:59 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/02/02 15:25:24 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t len;
+	size_t	i;
+	size_t	slen;
+	size_t	dlen;
 
-	return (len);
+	slen = ft_strlen(src);
+	if (dstsize == 0)
+		return (slen);
+	dlen = ft_strlen(dst);
+	if (dlen >= dstsize)
+		return (dstsize + slen);
+	i = 0;
+	while (dstsize > (dlen + i + 1) && src[i])
+	{
+		dst[dlen + i] = src[i];
+		i++;
+	}
+	dst[dlen + i] = '\0';
+	return (slen + dlen);
 }
