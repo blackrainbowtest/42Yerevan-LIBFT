62eda1f9897c4369643e2b681595efc69a87e0a6
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-02-03 19:41:13 +0400

    modified: norm fix:

--- dev/ft_strncmp.c
+++ dev/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 17:11:15 by aramarak          #+#    #+#             */
-/*   Updated: 2025/02/02 18:07:24 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/02/03 19:17:55 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = 0;
 	if (n != 0)
+	{
 		while ((i < n) && (s1[i] || s2[i]))
 		{
 			if (s1[i] != s2[i])
 				return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 			i++;
 		}
+	}
 	return (0);
 }
