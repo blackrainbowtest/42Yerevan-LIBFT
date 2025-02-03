1243e45aa3c27c506395b9b1184cdfbf14399eee
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-02-03 19:42:08 +0400

    modified: fix search logic

--- dev/ft_memchr.c
+++ dev/ft_memchr.c
@@ -6,20 +6,21 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 18:14:45 by aramarak          #+#    #+#             */
-/*   Updated: 2025/02/02 18:46:33 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/02/03 19:16:18 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((unsigned char *)s + i);
 		i++;
 	}
 	return (NULL);
