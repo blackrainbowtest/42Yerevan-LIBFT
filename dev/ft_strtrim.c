e30ba697e639a17be7b611630ec444dbe9cb50a8
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-02-06 12:06:51 +0400

    bugfix

--- dev/ft_strtrim.c
+++ dev/ft_strtrim.c
@@ -2,26 +2,20 @@
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
-	size_t	size;
+	size_t	start;
+	size_t	end;
 	size_t	len;
 	char	*strtrim;
 
 	if (!s1 || !set)
 		return (NULL);
-	size = ft_strlen(s1) - 1;
-	while (*s1)
-	{
-		if (!ft_strchr(set, *s1))
-			break;
-		s1++;
-	}
-	while (s1[size])
-	{
-		if (!ft_strchr(set, s1[size]))
-			break;
-		size--;
-	}
-	len = ft_strlen(ft_substr(s1, 0, size));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	len = end - start;
 	strtrim = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!strtrim)
 		return (NULL);
