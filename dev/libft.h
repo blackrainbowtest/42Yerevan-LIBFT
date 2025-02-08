cfc1cd3528c01b3f0961e5d89091952a4bd15ec9
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-02-08 23:44:40 +0400

    bugfix

--- dev/libft.h
+++ dev/libft.h
@@ -62,4 +62,13 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int i, int fd);
+
+// Bonus part
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+t_list	*ft_lstnew(void *content);
 #endif
