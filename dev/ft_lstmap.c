fc6bee92bbeec470188c4bf8b130f9d14a61ddbf
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-02-12 09:59:14 +0400

    first updates

--- dev/ft_lstmap.c
+++ dev/ft_lstmap.c
@@ -4,15 +4,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*tmp;
 	t_list	*new_list;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
-	tmp = lst;
-	while (tmp)
+	while (lst)
 	{
-		
-		tmp = tmp->next;
+		content = (*f)(lst->content);
+		tmp = ft_lstnew(content);
+		if (!tmp)
+		{
+			ft_lstclear(tmp, del);
+			return (NULL);
+		}
+		ft_lstadd_back(new_list, tmp);
+		lst = lst->next;
 	}
 	return (new_list);
 }
