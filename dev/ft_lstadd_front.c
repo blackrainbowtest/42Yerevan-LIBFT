40f4fd8e7d2dcb7f61f53ec74d87c5b66f1d44e9
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-02-14 21:10:59 +0400

    add

--- dev/ft_lstadd_front.c
+++ dev/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: aramarak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 14:22:41 by aramarak          #+#    #+#             */
-/*   Updated: 2025/02/09 14:27:40 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/02/14 20:40:25 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,18 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 		return ;
 	new->next = *lst;
 	*lst = new;
+}
+
+#include <stdio.h>
+int	main()
+{
+	t_list	*p_lst1;
+	t_list	*p_lst2;
+
+	p_lst1 = ft_lstnew("Head");
+	p_lst2 = ft_lstnew("Tail");
+	ft_lstadd_front(&p_lst1, p_lst2);
+	printf("%s\n", (char *)p_lst1->content);
+
+	return (0);
 }
